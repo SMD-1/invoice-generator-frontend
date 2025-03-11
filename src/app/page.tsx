ebd52048ba609be6ef1c189446dc8572bff9483c
SMD-1 <skdanish1999@gmail.com>
2025-03-11 23:48:27 +0530

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="dark">
+    <div className="dark mt-15">
       <Link href="/login">Login</Link>
     </div>
   );
